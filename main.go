53de92ff728c2c08b8653041c9599ee87ef19811
Marco Gregorius <marco@99s-mbp-4.lan>
2021-07-19 16:38:18 +0800

    chore(cleanup): remove unused codes

--- main.go
+++ main.go
@@ -19,32 +19,4 @@ func main() {
 
 	a.Initialize()
 	a.Run(port)
-
-	/*
-		db := models.InitDb()
-		defer db.Close()
-		r := router.Router()
-
-		srv := &http.Server{
-			Addr:    port,
-			Handler: r,
-			// Good practice to set timeouts to avoid Slowloris attacks.
-			WriteTimeout: time.Second * 15,
-			ReadTimeout:  time.Second * 15,
-			IdleTimeout:  time.Second * 60,
-		}
-		go func() {
-			//if err := http.ListenAndServe(port, r); err != nil {
-			if err := srv.ListenAndServe(); err != nil {
-				log.Fatal(err)
-			}
-		}()
-		log.Printf("Listening on port %s", port)
-
-		// make channel that will block until we receive SIGINT or SIGTERM
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-		<-c
-		log.Println("Shutting down")
-	*/
 }
